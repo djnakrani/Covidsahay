a0e08783ebe5907a27573d539c737f766e6ea71e
Dharmik Nakrani <dharmiknakrani1690@gmail.com>
2021-05-31 22:55:40 +0530

    all Home page linked give

--- user/urls.py
+++ user/urls.py
@@ -2,6 +2,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('',views.django_index),
+    path('',views.django_index,name='index'),
+    path('About_Us/',views.django_about,name='about-us'),
+    path('Activities/',views.django_activities,name='activities'),
+    path('Projects/',views.django_projects,name='projects'),
+    path('Gallery/',views.django_gallery,name='gallery'),
+    path('Contact_us/',views.django_gallery,name='contact-us'),
     
 ]
--- user/views.py
+++ user/views.py
@@ -2,6 +2,20 @@ from django.shortcuts import render
 
 # Create your views here.
 def django_index(request):
-     return render(request,'index.html')
+     return render(request,'view/index.html')
 
+def django_about(request):
+     return render(request,'view/about-us.html')
+
+def django_activities(request):
+     return render(request,'view/activities.html')
+
+def django_projects(request):
+     return render(request,'view/projects.html')
+
+def django_gallery(request):
+     return render(request,'view/gallery.html')
+
+def django_contact(request):
+     return render(request,'view/contact.html')
 
