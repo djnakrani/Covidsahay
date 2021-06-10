b8ad024dcd59bfe54ec8e19b03bbdf97a31c4c24
kashyap <kashyapkalathiya654@gmail.com>
2021-06-10 20:23:05 +0530

    Chanegs

--- user/models.py
+++ user/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Contact(models.Model):
+   name = models.CharField(max_length=20, default='', null=False)
+   email = models.CharField(max_length=40, default='', null=False)
+   subject = models.CharField(max_length=20, default='', null=False)
+   message = models.CharField(max_length=50, default='', null=False)
