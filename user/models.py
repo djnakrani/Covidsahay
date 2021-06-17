2f58f11783c5771bd4fda8d2712ccb01a173d230
kashyap <kashyapkalathiya654@gmail.com>
2021-06-17 18:49:39 +0530

    Login/Logout Session

--- user/models.py
+++ user/models.py
@@ -1,8 +1,24 @@
+
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+   fName = models.CharField(max_length=20, null=True)
+   lName = models.CharField(max_length=20, null=True)
+   mono = models.BigIntegerField(null=True)
+   gender = models.CharField(max_length=10, null=True)
+   dob = models.CharField(max_length=10, null=True)
+   bGrp = models.CharField(max_length=10, null=True)
+   email = models.CharField(max_length=30, null=True)
+   state = models.CharField(max_length=20, null=True)
+   city = models.CharField(max_length=20, null=True)
+   area = models.CharField(max_length=20, null=True)
+   add = models.CharField(max_length=50, null=True)
+   pwd = models.CharField(max_length=50, null=True)
+
+
 class Contact(models.Model):
-   name = models.CharField(max_length=20, default='', null=False)
-   email = models.CharField(max_length=40, default='', null=False)
-   subject = models.CharField(max_length=20, default='', null=False)
-   message = models.CharField(max_length=50, default='', null=False)
+   name = models.CharField(max_length=20, null=True)
+   email = models.CharField(max_length=40, null=True)
+   subject = models.CharField(max_length=20, null=True)
+   message = models.CharField(max_length=50, null=True)
