bc151919af5a7587f625adcb14340413b9a7fc27
Dharmik Nakrani <dharmiknakrani1690@gmail.com>
2021-07-01 00:15:29 +0530

    Add Admin Login UI

--- myadmin/views.py
+++ myadmin/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 
 # Create your views here.
 def django_admin(request):
-     return render(request, 'admin/admin.html')
+     return render(request, 'admin/login.html')
