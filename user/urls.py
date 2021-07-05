cd097e742d0d0e5ea8b5d1528b5b60c533b797a5
Dharmik Nakrani <dharmiknakrani1690@gmail.com>
2021-07-05 18:21:38 +0530

    Admin-Request Processing2

--- user/urls.py
+++ user/urls.py
@@ -3,11 +3,11 @@ from . import views
 
 urlpatterns = [
     path('', views.django_index, name='index'),
-    path('login/', views.django_login, name='login'),
-    path('register/', views.django_register, name='register'),
-    path('about_Us/', views.django_about, name='about-us'),
-    path('activities/', views.django_activities, name='activities'),
-    path('gallery/', views.django_gallery, name='gallery'),
-    path('contact_us/', views.django_contact, name='contact-us'),
-    path('request/', views.django_request, name='request'),
+    path('login', views.django_login, name='login'),
+    path('register', views.django_register, name='register'),
+    path('about_Us', views.django_about, name='about-us'),
+    path('activities', views.django_activities, name='activities'),
+    path('gallery', views.django_gallery, name='gallery'),
+    path('contact_us', views.django_contact, name='contact-us'),
+    path('request', views.django_request, name='request'),
 ]
