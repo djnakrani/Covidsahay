939d542b9928172fc84c8652a8e53b43fcaa8b8b
kashyap <kashyapkalathiya654@gmail.com>
2021-07-13 23:31:34 +0530

    Donate Logout etc

--- myadmin/urls.py
+++ myadmin/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('', django_admin_panel, name='alogin'),
+    path('/', django_admin_panel, name='alogin'),
     path('/Dashboard', django_admin_dashboard, name='admindashboard'),
     path('/alluser', django_admin_alluser, name='alluser'), 
     path('/Request', django_admin_request, name='request2'), 
